97c83d328dc905d8ba095d4ab9e9ef9df29bda66
Hedinyer Perucho <hedinyer.perucho@ustabuca.edu.co>
2021-02-22 10:11:56 -0500

    keyboard input commit

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main() 
+{
+    char c = 0;
+    char str[100];
+    memset(str, 0, sizeof(str));
+    while(c != '\n')
+    {
+        c=getchar();
+        if(c=!0xff)
+        {
+            str[strlen(str)]=c;
+            printg("%c",c);
+
+        }
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+    printf("you typed: %s\n",str);
+    
+}
+
+
